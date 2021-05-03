060b8b9e0dda48a35c386a3e088e010ed3b2d9fd
Kaushik Garikipati <kaushikg94@gmail.com>
2021-05-03 18:31:14 +0530

    Enable filtering and sorting results

--- VaccineFinderLibrary/Controllers/SlotFetchController.cs
+++ VaccineFinderLibrary/Controllers/SlotFetchController.cs
@@ -25,7 +25,44 @@ namespace VaccineFinderLibrary.Controllers
         [HttpPost]
         public async Task<CalendarResponse> PostAsync(CalendarRequest request)
         {
-            return await apiHandler.CalendarDistrictSearchAsync(request.state_name, request.district_name, request.date);
+            CalendarResponse apiResponse = await apiHandler.CalendarDistrictSearchAsync(request.state_name, request.district_name, request.date);
+            CalendarResponse filtered = FilterResults(apiResponse, request);
+            CalendarResponse sorted = SortResults(filtered);
+            return sorted;
+        }
+
+        private CalendarResponse FilterResults(CalendarResponse response, CalendarRequest request)
+        {
+            IEnumerable<Center> filteredCenters = response.centers;
+
+            foreach(var center in filteredCenters)
+            {
+                center.sessions = center.sessions.Where(session => session.available_capacity > 0);
+
+                if (request.min_age_limit != 0)
+                {
+                    center.sessions = center.sessions.Where(session => session.min_age_limit <= request.min_age_limit);
+                }
+                if (!string.IsNullOrEmpty(request.vaccine))
+                {
+                    center.sessions = center.sessions.Where(session => session.vaccine == request.vaccine);
+                }
+
+            }
+            filteredCenters = filteredCenters.Where(center => center.sessions.Any());
+
+            if (!string.IsNullOrEmpty(request.fee_type))
+            {
+                filteredCenters = filteredCenters.Where(center => center.fee_type == request.fee_type);
+            }
+
+            return new CalendarResponse { centers = filteredCenters };
+        }
+
+        private CalendarResponse SortResults(CalendarResponse response)
+        {
+            response.centers = response.centers.ToList().OrderBy(center => center.sessions.Select(session => session.date).Min());
+            return response;
         }
     }
 }
--- VaccineFinderLibrary/Models/Session.cs
+++ VaccineFinderLibrary/Models/Session.cs
@@ -6,8 +6,8 @@ namespace VaccineFinderLibrary.Models
     {
         public string session_id { get; set; }
         public string date { get; set; }
-        public int available_capacity { get; set; }
-        public int min_age_limit { get; set; }
+        public int available_capacity { get; set; } = 0;
+        public int min_age_limit { get; set; } = 0;
         public string vaccine { get; set; }
         public IEnumerable<string> slots { get; set; }
     }
